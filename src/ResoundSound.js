e8408d480794a95fafe3245a05409122a319df59
Caleb Hugo <calebjedhugo@gmail.com>
2023-11-23 22:56:00 -0600

    Building out gain node functionality

--- src/ResoundSound.js
+++ src/ResoundSound.js
@@ -6,30 +6,73 @@ import SoundManager from './SoundManager';
  */
 class ResoundSound {
   /**
-   * @param {"sine"|"triangle"|"sawtooth"} instrument
+   * @param {object} param0
+   * @param {"sine"|"triangle"|"sawtooth"} param0.type
    */
-  constructor(instrument = 'sine') {
+  constructor({ type = 'sine', transients = {} }) {
+    this.setSoundscape();
+    this.transients = transients;
+    this.type = type;
+  }
+
+  setSoundscape() {
     if (!window.resoundSoundscape) {
       const soundManager = new SoundManager();
       soundManager.verifySoundUnlocked();
       window.resoundSoundscape = soundManager.soundscape;
     }
     this.soundScape = window.resoundSoundscape;
+  }
+
+  connectOscillator() {
     this.oscillator = this.soundScape.createOscillator();
-    this.oscillator.type = instrument;
+    this.oscillator.connect(this.soundScape.destination);
+    this.oscillator.type = this.type;
+  }
+
+  connectGainNode() {
+    this.gainNode = this.soundScape.createGain();
+    this.oscillator.connect(this.gainNode);
+    this.gainNode.connect(this.soundScape.destination);
   }
 
   setPitch(pitch) {
-    console.log(pitchToFrequency(pitch));
     this.oscillator.frequency.setValueAtTime(
       pitchToFrequency(pitch),
       this.soundScape.currentTime
     );
   }
 
-  play({ length = 3000, pitch = 'A4' } = {}) {
-    this.oscillator.connect(this.soundScape.destination);
+  setVolume({ dynamic = 'mf', length }) {
+    const lengthInSeconds = length / 1000;
+    const { attack = 0.02, decay = 0.2, release = 0.05 } = this.transients;
+    const {
+      gainNode: { gain },
+      soundScape: { currentTime },
+    } = this;
+    const gainLevel = 1; // do something with dynamics here
+    gain.setValueAtTime(0, currentTime);
+    gain.linearRampToValueAtTime(gainLevel, currentTime + attack);
+    gain.linearRampToValueAtTime(
+      gainLevel * Math.exp(-decay * lengthInSeconds),
+      currentTime + attack + lengthInSeconds
+    );
+    gain.linearRampToValueAtTime(
+      0,
+      currentTime + attack + lengthInSeconds + release
+    );
+  }
+
+  play({ length = 3000, pitch = 'A4', dynamic, articulation } = {}) {
+    if (this.oscillator) {
+      this.oscillator.stop();
+    }
+    this.connectOscillator();
     this.setPitch(pitch);
+    this.connectGainNode();
+    this.setVolume({ dynamic, length });
+
+    // Start the playback
     this.oscillator.start();
 
     setTimeout(() => {
