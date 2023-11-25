f001fb0cf099a87dde388ec562f5243cd70557ad
Caleb Hugo <calebjedhugo@gmail.com>
2023-11-25 13:36:42 -0600

    Allow two notes at the same time

--- src/ResoundSound.js
+++ src/ResoundSound.js
@@ -13,6 +13,12 @@ class ResoundSound {
     this.setSoundscape();
     this.transients = transients;
     this.type = type;
+    this.soundingNodes = {};
+    this.stopTimouts = {};
+    this.primedNodeSet = {};
+
+    // get ready to play the first note.
+    this.prime();
   }
 
   setSoundscape() {
@@ -25,30 +31,30 @@ class ResoundSound {
   }
 
   /**
-   * Creates the oscillator, sets it to this.oscillator, and connects it to passed value
-   * @param {object} connectTo - the node that the oscillator will connect to. Defaults to this.soundScape.destination.
-   * @returns {object} the constructed oscillator
-   */
-  connectOscillator(connectTo = this.soundScape.destination) {
-    this.oscillator = this.soundScape.createOscillator();
-    this.oscillator.connect(connectTo);
-    this.oscillator.type = this.type;
-    return this.oscillator;
-  }
-
-  /**
    * Creates the gain node, sets it to this.gainNode, and connects it to passed value
    * @param {object} connectTo  - the node that the oscillator will connect to. Defaults to this.soundScape.destination.
    * @returns {object} the constructed gain node
    */
   connectGainNode(connectTo = this.soundScape.destination) {
-    this.gainNode = this.soundScape.createGain();
-    this.gainNode.connect(connectTo);
-    return this.gainNode;
+    this.primedNodeSet.gainNode = this.soundScape.createGain();
+    this.primedNodeSet.gainNode.connect(connectTo);
+    return this.primedNodeSet.gainNode;
+  }
+
+  /**
+   * Creates the oscillator, sets it to this.oscillator, and connects it to passed value
+   * @param {object} connectTo - the node that the oscillator will connect to. Defaults to this.soundScape.destination.
+   * @returns {object} the constructed oscillator
+   */
+  connectOscillator(connectTo = this.soundScape.destination) {
+    this.primedNodeSet.oscillator = this.soundScape.createOscillator();
+    this.primedNodeSet.oscillator.connect(connectTo);
+    this.primedNodeSet.oscillator.type = this.type;
+    return this.primedNodeSet.oscillator;
   }
 
   setPitch(pitch) {
-    this.oscillator.frequency.setValueAtTime(
+    this.primedNodeSet.oscillator.frequency.setValueAtTime(
       pitchToFrequency(pitch),
       this.soundScape.currentTime
     );
@@ -58,7 +64,9 @@ class ResoundSound {
     const lengthInSeconds = length / 1000;
     const { attack = 0.02, decay = 0.025, release = 0.05 } = this.transients;
     const {
-      gainNode: { gain },
+      primedNodeSet: {
+        gainNode: { gain },
+      },
       soundScape: { currentTime },
     } = this;
     const gainLevel = 1; // do something with dynamics here
@@ -82,28 +90,49 @@ class ResoundSound {
   }
 
   prime() {
+    // clear out the old object
+    this.primedNodeSet = {};
+
     // Connect gain node to the soundscape
-    this.connectGainNode();
+    const gainNode = this.connectGainNode();
 
     // Connect oscillator to the gain node
-    this.connectOscillator(this.gainNode);
+    this.connectOscillator(gainNode);
+  }
+
+  stop(pitch) {
+    if (pitch) {
+      if (this.soundingNodes[pitch]) {
+        this.soundingNodes[pitch].oscillator.stop();
+      } // else do nothing
+    } else
+      Object.values(this.soundingNodes).forEach(({ oscillator }) =>
+        oscillator.stop()
+      );
   }
 
   play({ length = 3000, pitch = 'A4', dynamic, articulation } = {}) {
-    if (this.oscillator) {
-      this.oscillator.stop();
-    }
-
-    this.prime();
     this.setVolume({ dynamic, length });
     this.setPitch(pitch);
 
-    // Start the playback
-    this.oscillator.start();
+    // Start the playback (This needs to happen asap!)
+    this.primedNodeSet.oscillator.start();
 
-    setTimeout(() => {
-      this.oscillator.stop();
+    // stop the playback of the same pitch
+    this.stop(pitch);
+
+    // clear the timeout for old node
+    clearTimeout(this.stopTimouts[pitch]);
+
+    // push the new node to soundingNodes
+    this.soundingNodes[pitch] = this.primedNodeSet;
+
+    this.stopTimouts[pitch] = setTimeout(() => {
+      this.stop(pitch);
     }, length);
+
+    // prepare for next note
+    this.prime();
   }
 }
 
--- src/editor/src/components/PlayButton.js
+++ src/editor/src/components/PlayButton.js
@@ -1,13 +1,14 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Piano } from 'resound-sound';
 
 const PlayButton = () => {
   const instrument = useMemo(() => new Piano(), []);
-
+  const [pitch, setPitch] = useState('C4');
   return (
     <button
       onClick={() => {
-        instrument.play({ length: 5000, pitch: 'C4' });
+        instrument.play({ length: 5000, pitch });
+        setPitch(pitch === 'C4' ? 'A4' : 'C4');
       }}
     >
       Play
